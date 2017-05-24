db6a05c64c337523f2f6cff8a54dc1ed206e1149
pipeline <pipeline@syncfusion.com>
2017-05-24 17:44:09 +0530

    v1.0.0-preview is released

--- src/chart/index.js
+++
@@ -1,8 +0,0 @@
-export { SeriesDirective, SeriessDirective } from './series.directive';
-export { AxisDirective, AxissDirective } from './axes.directive';
-export { RowDirective, RowsDirective } from './rows.directive';
-export { ColumnDirective, ColumnsDirective } from './columns.directive';
-export { ChartComponent, LineSeriesService, ScatterSeriesService, ColumnSeriesService, SplineSeriesService, AreaSeriesService, StepLineSeriesService, StackingColumnSeriesService, StackingAreaSeriesService, BarSeriesService, StackingBarSeriesService, TooltipService, CrosshairService, CategoryService, DateTimeService, LogarithmicService, MarkerService, LegendService, ZoomService, DataLabelService, SelectionService } from './chart.component';
-export { ChartModule } from './chart.module';
-export { ChartAllModule } from './chart-all.module';
-//# sourceMappingURL=index.js.map
--- src/index.js
+++
@@ -1,9 +0,0 @@
-export * from '@syncfusion/ej2-charts';
-export { SeriesDirective, SeriessDirective } from './chart/series.directive';
-export { AxisDirective, AxissDirective } from './chart/axes.directive';
-export { RowDirective, RowsDirective } from './chart/rows.directive';
-export { ColumnDirective, ColumnsDirective } from './chart/columns.directive';
-export { ChartComponent, LineSeriesService, ScatterSeriesService, ColumnSeriesService, SplineSeriesService, AreaSeriesService, StepLineSeriesService, StackingColumnSeriesService, StackingAreaSeriesService, BarSeriesService, StackingBarSeriesService, TooltipService, CrosshairService, CategoryService, DateTimeService, LogarithmicService, MarkerService, LegendService, ZoomService, DataLabelService, SelectionService } from './chart/chart.component';
-export { ChartModule } from './chart/chart.module';
-export { ChartAllModule } from './chart/chart-all.module';
-//# sourceMappingURL=index.js.map
